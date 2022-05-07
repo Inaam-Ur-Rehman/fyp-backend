44942c65aa7793980188c3cbb3851d44d867ee92
Muhammad Inaam Ur Rehman <inaamurrehman343@gmail.com>
2022-05-07 21:41:51 +0500

    make it bett

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get("/suppliers", suppliersRoute.getSuppliers)
 app.get("/sales", salesRoute.getSales)
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 })
